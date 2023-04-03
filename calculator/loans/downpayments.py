6d664f2df594ce1155f9f9799a21167b1ae73ebc
adamhrf <adamhrf@ntc.is>
2023-04-03 23:57:25 +0000

    Stuff

--- calculator/loans/downpayments.py
+++ calculator/loans/downpayments.py
@@ -9,11 +9,18 @@ class DownPayments:
         self.loan_context = loan_context
         self.rate_context = rate_context
 
-    def equal_payments(self, date: datetime.datetime) -> float:
+    def equal_instalments(self, date: datetime.datetime) -> float:
         return self.loan_context.capital / self.loan_context.n_payments
 
-    def equal_capital_payment(self, date: datetime.datetime) -> float:
+    def equal_payments(self, date: datetime.datetime) -> float:
         r = self.rate_context.get_monthly_rate(date)
         a = 1 + r
         b = (1 + r)**self.loan_context.n_payments
         return self.loan_context.capital*b*r/(b - 1)
+
+    def calculate_equal_instalments(self, capital: float, n_payments: int, rate: float):
+        return capital / n_payments
+
+    def calculate_equal_payments(self, capital: float, n_payments: int, rate: float):
+        b = (1 + rate)**n_payments
+        return capital*b*rate/(b - 1)
--- calculator/loans/indexedloan.py
+++ calculator/loans/indexedloan.py
@@ -14,21 +14,34 @@ class IndexBasedLoan:
         capital = self.context_payments.loan_context.capital
         loan_date = self.context_payments.loan_context.date_of_loan
         rate = self.context_payments.rate_context.get_monthly_rate(loan_date)
-        payment_base = self.context_payments.equal_capital_payment(loan_date)
-        inflation_base = self.context_inflation.get_monthly(loan_date)
+        payment_base = self.context_payments.equal_payments(loan_date)
         
         arr_capital = []
         arr_dates = []
         arr_payments = []
         arr_interest = []
         arr_instalments = []
-        
+        arr_extra_payments = []
+        arr_cumulative_inflation = []
+        cumulative_inflation = 0
         while capital >= 0:
-            arr_capital.append(capital)
             arr_dates.append(loan_date)
-            arr_payments.append(payment_base)
+            cumulative_inflation = self.context_inflation.get_monthly(arr_dates[-1])
+
+            arr_cumulative_inflation.append(cumulative_inflation)
+            arr_capital.append(capital * (1 + arr_cumulative_inflation[-1]))
+
+            arr_payments.append(
+                self.context_payments.calculate_equal_payments(
+                    arr_capital[-1],
+                    max(self.context_payments.loan_context.n_payments - (len(arr_capital) - 1), 1),
+                    self.context_payments.rate_context.get_monthly_rate(arr_dates[-1])
+                )
+            )
+
             arr_interest.append(arr_capital[-1] * rate)
             arr_instalments.append(payment_base - arr_interest[-1])
+            arr_extra_payments.append(0)
             
             loan_date = DateHandler.increment_by_month(loan_date)
             capital -= arr_instalments[-1]
@@ -36,8 +49,10 @@ class IndexBasedLoan:
         self.df = pd.DataFrame(
             dict(
                 date=arr_dates,
+                cumulative_inflation=arr_cumulative_inflation,
                 capital_base=arr_capital,
                 payments_base=arr_payments,
+                payments_extra=arr_extra_payments,
                 interests_base=arr_interest,
                 instalments_base=arr_instalments
             )
@@ -45,12 +60,17 @@ class IndexBasedLoan:
         
         return self.df
 
+    def recalculate(self):
+        # Re-adjust capital based on extra payments made 
+        self.df['payments_base'] = self.df.apply(lambda row: self.context_payments.calculate_equal_payments(
+            row['capital_base'],
+            self.context_payments.loan_context.n_payments - row.name,
+            self.context_payments.rate_context.get_monthly_rate(row['date'])
+        ), axis=1)
+
     def calculate(self):
-        self.df['inflation_csum'] = self.df['date'].apply(
-            lambda x: self.context_inflation.get_monthly(x)
-        ).cumsum()
+        self.df['inflation_csum'] = self.df['monthly_inflation'].cumsum()
         self.df['capital'] = self.df['capital_base'] * (1 + self.df['inflation_csum'])
         self.df['payment'] = self.df['payments_base'] * (1 + self.df['inflation_csum'])
         self.df['interest'] = self.df['interests_base'] * (1 + self.df['inflation_csum'])
         self.df['instalments'] = self.df['instalments_base'] * (1 + self.df['inflation_csum'])
-    
